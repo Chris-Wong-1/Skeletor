4585e34cbdab2d3efe74e53ac824b475562ee508
Chris Wong <email": null>
2016-06-28 10:34:17 -0700

    Add profile and home page

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,5 +1,5 @@
 get '/sessions/new' do
-	
+
 	erb :'/sessions/new'
 end
 
@@ -7,7 +7,7 @@ post '/sessions' do
 	@user = User.find_by(email: params[:email])
 	if @user && @user.password == params[:password]
 		session[:user_id] = @user.id
-		redirect '/'
+		redirect "/users/#{session[:user_id]}"
 	else
 		@error = "Email and password do not match"
 		redirect '/sessions/new'
